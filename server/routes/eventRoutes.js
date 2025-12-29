6328b686ac6e0612c24ad39f2db81e1684966a43
Ranjithbabu0912 <ranjithbabu0912@gmail.com>
2025-12-29 18:08:27 +0530

    token added

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -3,15 +3,12 @@
     createRegistration,
     checkPaymentStatus,
   } from "../controllers/eventController.js";
-  import { attachUser } from "../middlewares/attachUser.js";
-
   const router = express.Router();
 
   router.post("/register", createRegistration);
 
   router.post(
     "/check-status",
-    attachUser,
     checkPaymentStatus
   );
   export default router;
