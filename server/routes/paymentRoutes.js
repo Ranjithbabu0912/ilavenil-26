c262fe952201a99a03278f7a57ce611d4402166a
Ranjithbabu0912 <ranjithbabu0912@gmail.com>
2025-12-29 21:59:23 +0530

    final one

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,18 +1,15 @@
 import express from "express";
 import upload from "../middlewares/upload.js";
 import { submitPayment } from "../controllers/paymentController.js";
-import { blockAdmin } from "../middlewares/blockAdmin.js";
-import { clerkProtect } from '../middlewares/authMiddleware.js';
-import { attachUser } from "../middlewares/attachUser.js";
+// import { blockAdmin } from "../middlewares/blockAdmin.js";
+// import { clerkProtect } from '../middlewares/authMiddleware.js';
+// import { attachUser } from "../middlewares/attachUser.js";
 
 const router = express.Router();
 
 router.post(
     "/manual/:id",
     upload.single("screenshot"),
-    clerkProtect,
-    attachUser,
-    blockAdmin,  
     submitPayment
 );
 
